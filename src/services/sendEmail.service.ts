d9acee29fa10e6aea3505f36438f8eb90c8eb222
pratikSondauleApera <pratik@aperasoftwares.com>
2024-06-26 17:04:41 +0530

    Handle error check

--- src/services/sendEmail.service.ts
+++ src/services/sendEmail.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as SendGrid from '@sendgrid/mail';
 
@@ -6,12 +6,19 @@ import * as SendGrid from '@sendgrid/mail';
 export class SendgridService {
     constructor(private readonly configService: ConfigService) {
         const apiKey = this.configService.get<string>('SENDGRID_API_KEY')
+
+        if (!apiKey) {
+            throw new HttpException('Api key not found', HttpStatus.NOT_FOUND)
+        }
         SendGrid.setApiKey(apiKey);
     }
 
     async send(mail: SendGrid.MailDataRequired) {
-        const transport = await SendGrid.send(mail);
-        console.log(`E-Mail sent to ${mail.to}`);
-        return transport;
+        try {
+            const transport = await SendGrid.send(mail);
+            return transport;
+        } catch (error) {
+            throw new HttpException('Something went wrong while sending email', HttpStatus.FORBIDDEN)
+        }
     }
 }
